dbded543affd6353bcc20a145d6a4fbb9a11487b
hamidrezadj <hamidrezadj.dy7mq@aleeas.com>
2025-06-06 19:48:22 +0330

    Add the ability to pick a nixos version for the system

--- flake.nix
+++ flake.nix
@@ -5,12 +5,18 @@
     { self }:
     let
       user = {
+        # Host name will be your system's identity, you should remember it.
+        hostName = "bridge_names_for_example";
+        # nixosVersion should be "stable" or "unstable" or any other string.
+        # This should be configured alongside dotfiles flake.nix file,
+        # so it exists on the other side as well.
+        nixosVersion = "stable";
+        # Version of last clean install of Nixos.
+        stateVersion = "23.11";
+
         name = "Your Name";
         email = "your.email.address@server.com";
         userName = "system_username";
-        hostName = "fictional_ship_name";
-        # Version of latest clean install of Nixos
-        stateVersion = "23.11";
         timeZone = "US/Pacific";
         # mkpasswd --method=bcrypt --rounds=13
         hashedPassword = "$2b$13$FXZQRzk5jxCpxFubcwlvnuNHvKM/nREu2MUJKlQkx20Fp91MsMU5S";
